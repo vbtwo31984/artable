03dd5240fb98ff1998b60d45c85fb3dbe51ae143
Vladimir Burmistrovich <vladimir@burmistrovich.com>
2019-07-14 14:40:40 -0400

    initial commit

---
+++ ArtableAdmin/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ArtableAdmin
+//
+//  Created by Vladimir Burmistrovich on 7/14/19.
+//  Copyright © 2019 Vladimir Burmistrovich. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
