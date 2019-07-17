89feb91ddd6642bb52b2f5b412114294f85c8367
Vladimir Burmistrovich <vbtwo31984@gmail.com>
2019-07-17 09:33:57 -0400

    allow log in

--- Artable/Controllers/LoginViewController.swift
+++ Artable/Controllers/LoginViewController.swift
@@ -7,24 +7,43 @@
 //
 
 import UIKit
+import Firebase
 
 class LoginViewController: UIViewController {
-
+    
+    @IBOutlet weak var emailText: UITextField!
+    @IBOutlet weak var passwordText: UITextField!
+    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func forgotPasswordPressed(_ sender: Any) {
     }
-    */
-
+    
+    @IBAction func loginPressed(_ sender: Any) {
+        guard let email = emailText.text, email.isNotEmpty,
+            let password = passwordText.text, password.isNotEmpty
+            else { return }
+        
+        activityIndicator.startAnimating()
+        
+        Auth.auth().signIn(withEmail: email, password: password) { (result, error) in
+            self.activityIndicator.stopAnimating()
+            
+            if let error = error {
+                debugPrint(error)
+                return
+            }
+            
+            print("signed in successfully")
+        }
+    }
+    
+    @IBAction func guestPressed(_ sender: Any) {
+    }
+    
 }
