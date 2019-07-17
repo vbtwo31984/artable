6b0fd81ad92d35110ebc7b3b52879c870e1bd43a
Vladimir Burmistrovich <vbtwo31984@gmail.com>
2019-07-17 09:10:54 -0400

    add rounded corner views

---
+++ Artable/Views/RoundedViews.swift
@@ -0,0 +1,34 @@
+//
+//  RoundedViews.swift
+//  Artable
+//
+//  Created by V.Burmistrovich on 07/17/2019.
+//  Copyright © 2019 Vladimir Burmistrovich. All rights reserved.
+//
+
+import UIKit
+
+class RoundedButton : UIButton {
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        layer.cornerRadius = 5
+    }
+}
+
+class RoundedShadowView : UIView {
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        layer.cornerRadius = 5
+        layer.shadowColor = Color.Blue.cgColor
+        layer.shadowOpacity = 0.4
+        layer.shadowOffset = CGSize.zero
+        layer.shadowRadius = 3
+    }
+}
+
+class RoundedImageView : UIImageView {
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        layer.cornerRadius = 5
+    }
+}
